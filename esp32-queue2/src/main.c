7b45dd9d73921c89b0b69d44374192e4a5f3f942
uo288120 <145131850+Pedro-C-M@users.noreply.github.com>
2026-02-20 13:46:50 +0100

    Ejercicio queue2

--- esp32-queue2/src/main.c
+++ esp32-queue2/src/main.c
@@ -2,6 +2,7 @@
 #include <freertos/task.h>
 #include <freertos/queue.h>
 #include "esp_log.h"
+#include "esp_random.h"
 
 #define TASK_STACK_SIZE                 2048
 #define TASK_ACQ_PRIORITY               5
@@ -37,25 +38,27 @@ void app_main()
     TaskHandle_t xHandleAcq[NUM_ACQ_SENSORS];
     TaskHandle_t xHandleDisp = NULL;    
     
-    xHTQueue = xQueueCreate(...);
+    xHTQueue = xQueueCreate(HT_QUEUE_LENGTH, sizeof(t_HTreading));
     if (xHTQueue == NULL)
     {
         ESP_LOGE(TAG, "Error creating queue. Restarting...");
         exit(EXIT_FAILURE);                
     }
-
+    static t_SensorParam params[NUM_ACQ_SENSORS];
     for (unsigned int i = 0; i < NUM_ACQ_SENSORS; i++)
     {
-        t_SensorParam param;
-        param.sensorID = i;
-        param.queue = xHTQueue;
+        params[i].sensorID = i;
+        params[i].queue = xHTQueue;
 
-        xTaskCreate(...);
+        char task_name[16];
+        sprintf(task_name, "TaskAcq_%d", i);
+        
+        xTaskCreate(HTAcquisition, task_name, TASK_STACK_SIZE, &params[i], TASK_ACQ_PRIORITY, &xHandleAcq[i]);
         configASSERT( xHandleAcq[i] );
         ESP_LOGI(TAG, "[app_main] Task_Acq %d created.", i);
     }
 
-    xTaskCreate(...);
+    xTaskCreate(HTDisplay, "TaskDisp", TASK_STACK_SIZE, xHTQueue, TASK_DISP_PRIORITY, &xHandleDisp);
     configASSERT( xHandleDisp );
     ESP_LOGI(TAG, "[app_main] Task_Disp created.");
 
@@ -80,7 +83,7 @@ void HTAcquisition(void * param)
 {
     TickType_t xLastWakeTime;
 
-    t_SensorParam SP = ...;    
+    t_SensorParam SP = *(t_SensorParam*)param;
 
     for (;;)
     {
@@ -92,7 +95,7 @@ void HTAcquisition(void * param)
         HT.temperature = esp_random() % 30 + 1;
         HT.humidity = esp_random() % 100;
 
-        xQueueSendToBack(...);
+        xQueueSendToBack(SP.queue, &HT, portMAX_DELAY);
 
         vTaskDelayUntil(&xLastWakeTime, HT_ACQ_RATE_MS / portTICK_PERIOD_MS);
     }
@@ -100,13 +103,13 @@ void HTAcquisition(void * param)
 
 void HTDisplay(void * queue)
 {
-    QueueHandle_t xQueue = ...;
+    QueueHandle_t xQueue = (QueueHandle_t)queue;
 
     for (;;)
     {
         t_HTreading HTreceived;
 
-        BaseType_t xStatus = xQueueReceive(...);
+        BaseType_t xStatus = xQueueReceive(xQueue, &HTreceived, portMAX_DELAY);
         if (xStatus == pdPASS)
         {
             printf("Sensor ID %d: Temperature %d°C, humidity %d%%\n", 
